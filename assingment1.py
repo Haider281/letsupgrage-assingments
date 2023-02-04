948c5cc8d6eb18c7cf07ebb7d2a3591e814b7b8e
Haider281 <83056506+Haider281@users.noreply.github.com>
2023-02-04 12:02:30 +0530

    assingment1

---
+++ assingment1.py
@@ -0,0 +1,10 @@
+import random
+
+x = random.randint(1, 3)
+
+if x == 1:
+  print("Today will be a lucky day")
+elif x == 2:
+  print("You will meet someone new and interesting")
+else:
+  print("Watch out for obstacles in your path")
